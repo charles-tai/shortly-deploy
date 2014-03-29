217f49a86d0438336b176952851839da5860ff1e
Hack Reactor Students <cwtai86@gmail.com>
2014-03-29 12:28:16 -0700

    Completed Enable navigation between views and validate links

--- public/angular.js
+++ public/angular.js
@@ -1,19 +1,39 @@
 
-var app = angular.module('app',[]);
+var app = angular.module('app',['ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+   when('/create', {
+    templateUrl: 'shortenForm.html',
+    controller: 'linkController'
+  }).
+   when('/', {
+    templateUrl: 'allLinks.html',
+    controller: 'linkController'
+  }).
+   otherwise({
+    redirectTo: '/login.html'
+  });
+}]);
+
 app.controller('linkController', function($scope,$http) {
   $scope.getLinks = function() {
     return $http({
       method: 'GET',
       url: '/links'
     }).then(function(res) {
-      return res.data;
+      $scope.links =res.data;
     });
-  }
+  };
   $scope.postLink = function () {
     console.log('post');
     return $http.post('/links', {url: $scope.data})
               .success(function(data) {
-                console.log('post success: ', data)
+                console.log('post success: ', data);
+                $scope.invalidUrl = false;
+              })
+              .error(function(data) {
+                console.log('failed');
+                $scope.invalidUrl = true;
               });
-  }
+  };
 });
